059b29eff35e6c51bc475dba0f6a2af53fbaf74d
Juho Rutila <juho.rutila@gmail.com>
2018-10-31 21:09:02 +0000

    Images to events

--- event/models.py
+++ event/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, InlinePanel
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 # Create your models here.
 
@@ -13,6 +14,7 @@ class EventPage(Page):
     end = models.DateTimeField("Event end date and time")
     intro = models.CharField(max_length=250)
     description = RichTextField(blank=True)
+    image = models.ForeignKey('wagtailimages.Image', on_delete=models.SET_NULL, related_name='+', null=True)
 
     # Editor panels configuration
 
@@ -21,6 +23,7 @@ class EventPage(Page):
             FieldPanel('start'),
             FieldPanel('end'),
         ]),
+        ImageChooserPanel('image'),
         FieldPanel('intro', classname="full"),
         FieldPanel('description', classname="full"),
     ]
