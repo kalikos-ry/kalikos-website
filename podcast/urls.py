7fcffad7513425186a28420bd42c52d620badf13
Juho Rutila <juho.rutila@gmail.com>
2020-08-24 06:17:47 +0000

    Initial podcast refresh button

---
+++ podcast/urls.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import url, include
+from django.urls import path
+from aldryn_django.utils import i18n_patterns
+import aldryn_addons.urls
+from wagtail.contrib.sitemaps.views import sitemap
+from views import RefreshView
+
+urlpatterns = [
+    path('refresh/', RefreshView.as_view(), 'podcast_podcast_modeladmin_refresh')
+]
