c024f14c1d3c0b1e9f3f69272f731541dd6c73a9
Juho Rutila <juho.rutila@iki.fi>
2023-03-21 00:57:37 +0200

    feat: more working browse and stuff

--- wagtail_phpbb/models.py
+++ wagtail_phpbb/models.py
@@ -1,26 +1,12 @@
 from django.db import models
 from home.normal_page import NormalPage
+from wagtail.contrib.routable_page.models import RoutablePageMixin, path
 from django.conf import settings
 import random
 import pymysql.cursors
 
-class ForumSearchPage(NormalPage):
-  def get_context(self, request, *args, **kwargs):
-    context = super().get_context(request, *args, **kwargs)
-    filter = {}
-    sort = request.GET.get('sort', random.randint(0, 1000))
-    filter['sort'] = sort
-
-    skip = int(request.GET.get('skip', 0))
-    page_size = 50
-
-    author = request.GET.get('author', None)
-    forum = request.GET.get('forum', None)
-    if author:
-      filter['author'] = int(author)
-    if forum:
-      filter['forum'] = int(forum)
-
+class ForumSearchPage(RoutablePageMixin, NormalPage):
+  def get_connection(self):
     forum_db = settings.FORUM_DATABASE
     forum_db = {k.lower(): v for k, v in forum_db.items()}
     del forum_db['name']
@@ -29,12 +15,44 @@ class ForumSearchPage(NormalPage):
     del forum_db['engine']
     forum_db['database'] = forum_db['user']
     connection = pymysql.connect(**forum_db)
+    return connection
 
+  def get_filter(self, request):
+    filter = {}
+
+    sort = request.GET.get('sort', random.randint(0, 1000))
+    filter['sort'] = sort
+
+    author = request.GET.get('author', None)
+    forum = request.GET.get('forum', None)
+    scope = request.GET.get('scope', None)
+    if author:
+      filter['author'] = int(author)
+    if forum:
+      filter['forum'] = int(forum)
+    if scope:
+      filter['scope'] = scope
+
+    return filter
+
+  @path('')
+  def browse(self, request):
+    filter = self.get_filter(request)
+    context = {}
+
+    sort = filter['sort']
+    skip = int(request.GET.get('skip', 0) or 0)
+    scope = request.GET.get('scope', None)
+    page_size = 50
+
+    connection = self.get_connection()
     with connection:
       with connection.cursor(pymysql.cursors.DictCursor) as cursor:
         params = ()
-        sql = """
+        if scope == 'post':
+          sql = """
 SELECT
+  p.post_id,
   t.topic_id,
   u.username,
   u.user_id,
@@ -42,6 +60,23 @@ SELECT
   from_unixtime(t.topic_time) AS topic_time,
   t.topic_replies,
   pt.post_text,
+  f.forum_id,
+  f.forum_name
+  FROM `phpbb_posts` AS p
+  INNER JOIN `phpbb_topics` AS t ON t.topic_id = p.topic_id
+  INNER JOIN `phpbb_users` AS u ON u.user_id = p.poster_id
+  INNER JOIN `phpbb_posts_text` AS pt ON pt.post_id = p.post_id
+  INNER JOIN `phpbb_forums` AS f ON f.forum_id = t.forum_id
+"""
+        else:
+          sql = """
+SELECT
+  t.topic_id,
+  u.username,
+  u.user_id,
+  t.topic_title,
+  from_unixtime(t.topic_time) AS topic_time,
+  t.topic_replies,
   f.forum_id,
   f.forum_name
   FROM `phpbb_topics` AS t
@@ -77,8 +112,63 @@ SELECT
         cursor.execute(sql, params)
         result = cursor.fetchall()
         context['result'] = result
-        context['filter'] = filter
-        context['page_size'] = page_size
-        context['next_skip'] = skip + page_size
 
+    context['filter'] = filter
+    context['page_size'] = page_size
+    context['next_skip'] = skip + page_size
+    context['skip'] = skip
+
+    return self.render(request, context_overrides=context,
+      template='wagtail_phpbb/forum_search_page.html')
+
+  @path('<int:topic_id>/')
+  def single_topic(self, request, topic_id):
+    context = {}
+    connection = self.get_connection()
+    with connection:
+      with connection.cursor(pymysql.cursors.DictCursor) as cursor:
+        params = ()
+        params += (topic_id,)
+        sql = """
+SELECT
+  t.topic_id,
+  u.username,
+  t.topic_title,
+  t.topic_first_post_id,
+  from_unixtime(t.topic_time) AS topic_time,
+  f.forum_id,
+  f.forum_name
+  FROM `phpbb_topics` AS t
+  INNER JOIN `phpbb_users` AS u ON u.user_id = t.topic_poster
+  INNER JOIN `phpbb_forums` AS f ON f.forum_id = t.forum_id
+  WHERE t.topic_id = %s
+"""
+        cursor.execute(sql, params)
+        result = cursor.fetchone()
+        context['topic'] = result
+
+        sql = """
+SELECT
+  t.topic_title,
+  p.post_id,
+  u.username,
+  u.user_id,
+  from_unixtime(p.post_time) AS post_time,
+  pt.post_text
+  FROM `phpbb_posts` AS p
+  INNER JOIN `phpbb_topics` AS t ON t.topic_id = p.topic_id
+  INNER JOIN `phpbb_users` AS u ON u.user_id = p.poster_id
+  INNER JOIN `phpbb_posts_text` AS pt ON pt.post_id = p.post_id
+  WHERE p.topic_id = %s
+"""
+        cursor.execute(sql, params)
+        result = cursor.fetchall()
+        context['result'] = result
+    return self.render(request, context_overrides=context,
+      template='wagtail_phpbb/forum_topic.html')
+
+  def get_context(self, request, *args, **kwargs):
+    context = super().get_context(request, *args, **kwargs)
+    context['filter'] = self.get_filter(request)
+    context['skip'] = int(request.GET.get('skip', 0) or 0)
     return context
