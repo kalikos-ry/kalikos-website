8c4e3d863e0de234898b9e342f8f93f1d17fe538
Juho Rutila <juho.rutila@gmail.com>
2024-02-26 10:05:02 +0200

    feat: use new membra api (v2)

--- apps/organization/forms.py
+++ apps/organization/forms.py
@@ -8,31 +8,33 @@ class MembraClient(APIClient):
     def __init__(self, *args, **kwargs):
         kwargs['authentication_method'] = HeaderAuthentication(
             token=settings.MEMBRA_API_TOKEN,
-            parameter="X-Membra-Api-Key",
+            parameter="X-Api-Key",
             scheme=None,
             )
         kwargs['response_handler'] = JsonResponseHandler
         super().__init__(*args, **kwargs)
         
     def add_member(self, data):
-        url = "https://api.membra.fi/public/v1/member"
+        url = "https://api.membra.fi/v2/member"
         mdata = {
-            'member_firstname': data['first_name'],
-            'member_lastname': data['last_name'],
-            'member_email': data['email'],
-            'member_status': 'APPLICANT',
-            'member_birthdate': '0001-01-01',
-            'member_street': '-',
-            'member_zipcode': 0,
-            'member_city': '-',
-            'member_sex': 'M',
-            'member_accept_email_invoice': True,
-            'member_entry_date': timezone.now().strftime('%Y-%m-%d')
+            'mem_firstname': data['first_name'],
+            'mem_lastname': data['last_name'],
+            'mem_email': data['email'],
+            'mef_id': '1',
+            'mem_street': '-',
+            'mem_sex': '-',
+            'mem_accept_email_invoice': 1,
+            'mes_entrydate': timezone.now().strftime('%Y-%m-%d'),
+            'mes_status': 5, # 5 = applicant
         }
         result = self.post(url, data=mdata)
-        if result['result_data']:
-            result = self.get(url + '/' + str(result['result_data']))
-        return result['result_data'][0]
+        if (result['http_status'] == 200):
+            return {
+                'id': result['data'],
+                'name': data['first_name'] + ' ' + data['last_name'],
+                'email': data['email'],
+            }
+        raise Exception("Failed to add member, contact support")
 
 class JoinOrganizationForm(forms.Form):
     first_name = forms.CharField(max_length=255, label='Etunimi')
@@ -44,7 +46,4 @@ class JoinOrganizationForm(forms.Form):
     def save(self):
         c = MembraClient()
         member = c.add_member(self.cleaned_data)
-        member['id'] = member['membership_id']
-        member['name'] = member['member_firstname'] + ' ' + member['member_lastname']
-        member['email'] = member['member_email']
         return member
