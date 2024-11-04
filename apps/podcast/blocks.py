444270fd8a6c743c56aebbad03ea05cf3b2037cb
Juho Rutila <juho.rutila@gmail.com>
2024-11-04 10:30:09 +0200

    fix: fix migration to podcast

--- apps/podcast/blocks.py
+++ apps/podcast/blocks.py
@@ -1,5 +1,5 @@
 from .views import podcast_chooser_viewset
 
 PodcastChooserBlock = podcast_chooser_viewset.get_block_class(
-    name="PodcastChooserBlock", module_path="podcast.blocks"
+    name="PodcastChooserBlock", module_path="apps.podcast.blocks"
 )
