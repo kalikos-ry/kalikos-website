db28a36ef129f1e389a54bdf2819b2f645d03081
Juho Rutila <juho.rutila@gmail.com>
2026-01-24 11:32:36 +0200

    whitenoise use finders

--- kalikos/settings.py
+++ kalikos/settings.py
@@ -174,6 +174,8 @@ STATICFILES_FINDERS = [
     'sass_processor.finders.CssFinder',
     'compressor.finders.CompressorFinder'
 ]
+WHITENOISE_USE_FINDERS = True
+WHITENOISE_AUTOREFRESH = DEBUG
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
